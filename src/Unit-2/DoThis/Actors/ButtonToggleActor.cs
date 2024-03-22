0a65ba672a31d6c0c647115338ff222e45a616e6
espen-servicewell <espen.lever@servicewell.se>
2024-03-22 13:56:30 +0100

    ButtonToggleActor

---
+++ src/Unit-2/DoThis/Actors/ButtonToggleActor.cs
@@ -0,0 +1,70 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChartApp.Actors
+{
+    /// <summary>
+    /// Actor responsible for managing button toggles
+    /// </summary>
+    public class ButtonToggleActor : UntypedActor
+    {
+        #region Message types
+        /// <summary>
+        /// Toggles this button on or off and sends an appropriate messages
+        /// to the <see cref="PerformanceCounterCoordinatorActor"/>
+        /// </summary>
+        public class Toggle { }
+        #endregion
+
+        private readonly CounterType _myCounterType;
+        private bool _isToggledOn;
+        private readonly Button _myButton;
+        private readonly IActorRef _coordinatorActor;
+
+        public ButtonToggleActor(CounterType myCounterType, bool isToggledOn = false, Button myButton, IActorRef coordinatorActor)
+        {
+            _myCounterType = myCounterType;
+            _isToggledOn = isToggledOn;
+            _myButton = myButton;
+            _coordinatorActor = coordinatorActor;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if (message is Toggle && _isToggledOn)
+            {
+                //toggle is currently on
+
+                //stop watching this counter
+                _coordinatorActor.Tell(new PerformanceCounterCoordinatorActor.Unwatch(_myCounterType));
+
+                FlipToggle();
+            }
+            else if (message is Toggle && !_isToggledOn)
+            {
+                // toggle is currently off
+
+                // start watching this counter
+                _coordinatorActor.Tell(new PerformanceCounterCoordinatorActor.Watch(_myCounterType));
+
+                FlipToggle();
+            }
+            else
+            {
+                Unhandled(message);
+            }
+        }
+
+        private void FlipToggle()
+        {
+            _isToggledOn = !_isToggledOn;
+
+            _myButton.Text = string.Format("{0} ({1})", 
+                _myCounterType.ToString().ToUpperInvariant(),
+                _isToggledOn ? "ON" : "OFF");
+        }
+    }
+}
